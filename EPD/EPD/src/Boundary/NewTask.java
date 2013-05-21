ec1850c1a62a03fc5cfcc326d2f3b22ce9985c1d
Luc Klaassen <luc.klaassen@gmail.com>
2013-05-21 14:31:05 +0200

    Content panel
    Content panel omgezet naar aparte klasse

--- EPD/EPD/src/Boundary/NewTask.java
+++ EPD/EPD/src/Boundary/NewTask.java
@@ -33,62 +33,29 @@ import oracle.jdeveloper.layout.XYLayout;
 
 public class NewTask extends JFrame {
     private TaskController tc;
-    private HashMap<String, String> hmEmployees;
-    private HashMap<String, String> hmLocations;
+    
+    private NewTaskPanel ntp;
+    
     private JPanel pnlLoggedIn = new JPanel();
     private XYLayout xYLayout6 = new XYLayout();
     private JLabel lblLoggedIn = new JLabel();
     private JLabel lblUsername = new JLabel();
     private JLabel lblDateTime = new JLabel();
     private JButton btnLogout = new JButton();
-    private JPanel pnlContent = new JPanel();
-    private JTextField txtStartDateTime = new JTextField();
-    private JTextField txtEndDateTime = new JTextField();
-    private JTextArea txtDescription = new JTextArea();
-    private JLabel lblDescription = new JLabel();
-    private JLabel lblStartDateTime = new JLabel();
-    private JLabel lblEndDateTime = new JLabel();
-    private JLabel lblCategorie = new JLabel();
-    private JLabel lblEmployee = new JLabel();
-    private JLabel lblLocation = new JLabel();
-    private DefaultListModel lmAvailableEmployees = new DefaultListModel();
-    private DefaultListModel lmSelectedEmployees = new DefaultListModel();
-    private JList listAvailableEmployees = new JList(lmAvailableEmployees);
-    private JList listSelectedEmployees = new JList(lmSelectedEmployees);
-    private JButton btnAddEmployee = new JButton();
-    private JButton btnRemoveEmployee = new JButton();
-    private JComboBox cbCategorie = new JComboBox();
-    private JComboBox cbLocatie = new JComboBox();
+    
     private JPanel pnlSecondary = new JPanel();
     private JButton btnAnnuleren = new JButton();
     private JButton btnSave = new JButton();
-    private JSeparator jSeparator1 = new JSeparator();
-    private JSeparator jSeparator2 = new JSeparator();
 
     public NewTask(TaskController tc) {
         
         this.tc = tc;
+        ntp = new NewTaskPanel(tc);
         
         try {
             jbInit();
         } catch (Exception e) {
             e.printStackTrace();
-        }
-        
-        hmEmployees = tc.getAvailableEmployees();
-        hmLocations = tc.getLocations();
-        for(String category : tc.getCategories()){
-            cbCategorie.addItem(category);
-        }
-        
-        for (Map.Entry<String, String> entry : hmEmployees.entrySet())
-        {
-            lmAvailableEmployees.addElement(entry.getKey());
-        }
-        
-        for (Map.Entry<String, String> entry : hmLocations.entrySet())
-        {
-            cbLocatie.addItem(entry.getKey());
         }
         
     }
@@ -98,91 +65,24 @@ public class NewTask extends JFrame {
         this.setSize(new Dimension(800, 600));
         pnlLoggedIn.setBounds(new Rectangle(0, 0, 800, 30));
         pnlLoggedIn.setLayout(xYLayout6);
-        pnlContent.setLayout(xYLayout6);
         pnlSecondary.setLayout(xYLayout6);
         lblLoggedIn.setText("Ingelogd: ");
         lblUsername.setText("Username");
         lblDateTime.setText("01-01-2001  00:00");
         btnLogout.setText("Uitloggen");
-        pnlContent.setBounds(new Rectangle(0, 30, 800, 470));
-        lblDescription.setText("Omschrijving");
-        lblStartDateTime.setText("Start datum  (dd-MM-yyyy hh:mm)");
-        lblEndDateTime.setText("Eind datum  (dd-MM-yyyy hh:mm)");
-        lblCategorie.setText("Categorie");
-        lblEmployee.setText("Medewerkers");
-        lblLocation.setText("Locatie");
-        btnAddEmployee.setText(">");
-        btnAddEmployee.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-                    btnAddEmployee_actionPerformed(e);
-                }
-            });
-        btnRemoveEmployee.setText("<");
-        btnRemoveEmployee.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-                    btnRemoveEmployee_actionPerformed(e);
-                }
-            });
         pnlSecondary.setBounds(new Rectangle(0, 500, 800, 80));
         btnAnnuleren.setText("Annuleren");
         btnSave.setText("Opslaan");
 
-        btnSave.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-                    btnSave_actionPerformed(e);
-                }
-            });
         pnlLoggedIn.add(lblDateTime, new XYConstraints(165, 10, 140, 15));
         pnlLoggedIn.add(lblLoggedIn, new XYConstraints(10, 10, 65, 15));
         pnlLoggedIn.add(lblUsername, new XYConstraints(65, 10, 80, 15));
         pnlLoggedIn.add(btnLogout, new XYConstraints(695, 5, 90, 20));
-        pnlContent.add(jSeparator2, new XYConstraints(0, 455, 800, -1));
-        pnlContent.add(jSeparator1, new XYConstraints(0, 5, 800, -1));
-        pnlContent.add(cbLocatie, new XYConstraints(205, 365, 200, 20));
-        pnlContent.add(cbCategorie, new XYConstraints(205, 220, 200, 20));
-        pnlContent.add(btnRemoveEmployee, new XYConstraints(410, 310, 45, 20));
-        pnlContent.add(btnAddEmployee, new XYConstraints(410, 270, 45, 20));
-        pnlContent.add(listSelectedEmployees,
-                       new XYConstraints(465, 250, 190, 105));
-        pnlContent.add(listAvailableEmployees,
-                       new XYConstraints(205, 250, 200, 105));
-        pnlContent.add(lblLocation, new XYConstraints(15, 365, 45, 15));
-        pnlContent.add(lblEmployee, new XYConstraints(10, 250, 130, 15));
-        pnlContent.add(lblCategorie, new XYConstraints(10, 220, 100, 15));
-        pnlContent.add(lblEndDateTime, new XYConstraints(10, 185, 200, 15));
-        pnlContent.add(lblStartDateTime, new XYConstraints(10, 150, 195, 15));
-        pnlContent.add(lblDescription, new XYConstraints(10, 45, 80, 35));
-        pnlContent.add(txtDescription, new XYConstraints(205, 55, 450, 80));
-        pnlContent.add(txtEndDateTime, new XYConstraints(205, 185, 200, 20));
-        pnlContent.add(txtStartDateTime, new XYConstraints(205, 150, 200, 20));
-        this.getContentPane().add(pnlContent, null);
+        
+        this.getContentPane().add(ntp);
         this.getContentPane().add(pnlLoggedIn, null);
         pnlSecondary.add(btnSave, new XYConstraints(655, 0, 130, 70));
         pnlSecondary.add(btnAnnuleren, new XYConstraints(10, 0, 130, 70));
         this.getContentPane().add(pnlSecondary, null);
-    }
-
-    private void btnAddEmployee_actionPerformed(ActionEvent e) {
-        lmSelectedEmployees.addElement(listAvailableEmployees.getSelectedValue());
-        lmAvailableEmployees.removeElement(listAvailableEmployees.getSelectedValue());
-    }
-
-    private void btnRemoveEmployee_actionPerformed(ActionEvent e) {
-        lmAvailableEmployees.addElement(listSelectedEmployees.getSelectedValue());
-        lmSelectedEmployees.removeElement(listSelectedEmployees.getSelectedValue());
-    }
-
-    private void btnSave_actionPerformed(ActionEvent e) {
-        ArrayList<String> selectedEmployees = new ArrayList<String>();
-        for (int i = 0; i < lmSelectedEmployees.size(); i++){
-            String empId = hmEmployees.get(lmSelectedEmployees.get(i).toString());
-            selectedEmployees.add(empId);
-            System.out.println(empId);
-        }
-        
-        String selectedLocationId = hmLocations.get(cbLocatie.getSelectedItem().toString());
-        System.out.println(selectedLocationId);
-        
-        
     }
 }
---
+++ EPD/EPD/src/Boundary/NewTaskPanel.java
@@ -0,0 +1,131 @@
+package Boundary;
+
+import Control.TaskController;
+
+import java.awt.Rectangle;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+
+import javax.swing.JSeparator;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+import oracle.jdeveloper.layout.XYConstraints;
+import oracle.jdeveloper.layout.XYLayout;
+
+public class NewTaskPanel extends JPanel {
+    
+    private TaskController tc;
+
+    private HashMap<String, String> hmEmployees;
+    private HashMap<String, String> hmLocations;
+    
+    private XYLayout xYLayout6 = new XYLayout();
+    private JTextField txtStartDateTime = new JTextField();
+    private JTextField txtEndDateTime = new JTextField();
+    private JTextArea txtDescription = new JTextArea();
+    private JLabel lblDescription = new JLabel();
+    private JLabel lblStartDateTime = new JLabel();
+    private JLabel lblEndDateTime = new JLabel();
+    private JLabel lblCategorie = new JLabel();
+    private JLabel lblEmployee = new JLabel();
+    private JLabel lblLocation = new JLabel();
+    private DefaultListModel lmAvailableEmployees = new DefaultListModel();
+    private DefaultListModel lmSelectedEmployees = new DefaultListModel();
+    private JList listAvailableEmployees = new JList(lmAvailableEmployees);
+    private JList listSelectedEmployees = new JList(lmSelectedEmployees);
+    private JButton btnAddEmployee = new JButton();
+    private JButton btnRemoveEmployee = new JButton();
+    private JComboBox cbCategorie = new JComboBox();
+    private JComboBox cbLocatie = new JComboBox();
+    
+    public NewTaskPanel(TaskController tc) {
+        
+        this.tc = tc;
+        
+        try {
+            jbInit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        hmEmployees = tc.getAvailableEmployees();
+        hmLocations = tc.getLocations();
+        for(String category : tc.getCategories()){
+            cbCategorie.addItem(category);
+        }
+        
+        for (Map.Entry<String, String> entry : hmEmployees.entrySet())
+        {
+            lmAvailableEmployees.addElement(entry.getKey());
+        }
+        
+        for (Map.Entry<String, String> entry : hmLocations.entrySet())
+        {
+            cbLocatie.addItem(entry.getKey());
+        }
+    }
+
+    private void jbInit() throws Exception {
+        
+        lblDescription.setText("Omschrijving");
+        lblStartDateTime.setText("Start datum  (dd-MM-yyyy hh:mm)");
+        lblEndDateTime.setText("Eind datum  (dd-MM-yyyy hh:mm)");
+        lblCategorie.setText("Categorie");
+        lblEmployee.setText("Medewerkers");
+        lblLocation.setText("Locatie");
+        btnAddEmployee.setText(">");
+        btnAddEmployee.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    btnAddEmployee_actionPerformed(e);
+                }
+            });
+        btnRemoveEmployee.setText("<");
+
+        btnRemoveEmployee.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    btnRemoveEmployee_actionPerformed(e);
+                }
+            });
+        this.setLayout( xYLayout6 );
+        this.setBounds(new Rectangle(0, 30, 800, 470));
+        this.add(cbLocatie, new XYConstraints(205, 365, 200, 20));
+        this.add(cbCategorie, new XYConstraints(205, 220, 200, 20));
+        this.add(btnRemoveEmployee, new XYConstraints(410, 310, 45, 20));
+        this.add(btnAddEmployee, new XYConstraints(410, 270, 45, 20));
+        this.add(listSelectedEmployees,
+                       new XYConstraints(465, 250, 190, 105));
+        this.add(listAvailableEmployees,
+                       new XYConstraints(205, 250, 200, 105));
+        this.add(lblLocation, new XYConstraints(15, 365, 45, 15));
+        this.add(lblEmployee, new XYConstraints(10, 250, 130, 15));
+        this.add(lblCategorie, new XYConstraints(10, 220, 100, 15));
+        this.add(lblEndDateTime, new XYConstraints(10, 185, 200, 15));
+        this.add(lblStartDateTime, new XYConstraints(10, 150, 195, 15));
+        this.add(lblDescription, new XYConstraints(10, 45, 80, 35));
+        this.add(txtDescription, new XYConstraints(205, 55, 450, 80));
+        this.add(txtEndDateTime, new XYConstraints(205, 185, 200, 20));
+        this.add(txtStartDateTime, new XYConstraints(205, 150, 200, 20));
+    }
+
+    private void btnAddEmployee_actionPerformed(ActionEvent e) {
+        lmSelectedEmployees.addElement(listAvailableEmployees.getSelectedValue());
+        lmAvailableEmployees.removeElement(listAvailableEmployees.getSelectedValue());
+    }
+
+    private void btnRemoveEmployee_actionPerformed(ActionEvent e) {
+        lmAvailableEmployees.addElement(listSelectedEmployees.getSelectedValue());
+        lmSelectedEmployees.removeElement(listSelectedEmployees.getSelectedValue());
+    }
+}
