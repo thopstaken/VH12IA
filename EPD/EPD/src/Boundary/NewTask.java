ee11c21076c689fb1f9d1263ef2e002d63f0a11e
Luc Klaassen <luc.klaassen@gmail.com>
2013-05-26 22:52:22 +0200

    Standardised panels

--- EPD/EPD/src/Boundary/NewTask.java
+++ EPD/EPD/src/Boundary/NewTask.java
@@ -1,5 +1,7 @@
 package Boundary;
 
+import Boundary.Common.Userpanel;
+
 import Control.TaskController;
 
 import java.awt.Component;
@@ -34,14 +36,10 @@ import oracle.jdeveloper.layout.XYLayout;
 public class NewTask extends JFrame {
     private TaskController tc;
     
+    private Userpanel userP;
     private NewTaskPanel ntp;
     
-    private JPanel pnlLoggedIn = new JPanel();
     private XYLayout xYLayout6 = new XYLayout();
-    private JLabel lblLoggedIn = new JLabel();
-    private JLabel lblUsername = new JLabel();
-    private JLabel lblDateTime = new JLabel();
-    private JButton btnLogout = new JButton();
     
     private JPanel pnlSecondary = new JPanel();
     private JButton btnAnnuleren = new JButton();
@@ -51,7 +49,7 @@ public class NewTask extends JFrame {
         
         this.tc = tc;
         ntp = new NewTaskPanel(tc);
-        
+        userP = new Userpanel("TestUser", )
         try {
             jbInit();
         } catch (Exception e) {
@@ -63,24 +61,15 @@ public class NewTask extends JFrame {
     private void jbInit() throws Exception {
         this.getContentPane().setLayout( null );
         this.setSize(new Dimension(800, 600));
-        pnlLoggedIn.setBounds(new Rectangle(0, 0, 800, 30));
-        pnlLoggedIn.setLayout(xYLayout6);
         pnlSecondary.setLayout(xYLayout6);
-        lblLoggedIn.setText("Ingelogd: ");
-        lblUsername.setText("Username");
-        lblDateTime.setText("01-01-2001  00:00");
-        btnLogout.setText("Uitloggen");
+        
         pnlSecondary.setBounds(new Rectangle(0, 500, 800, 80));
         btnAnnuleren.setText("Annuleren");
         btnSave.setText("Opslaan");
-
-        pnlLoggedIn.add(lblDateTime, new XYConstraints(165, 10, 140, 15));
-        pnlLoggedIn.add(lblLoggedIn, new XYConstraints(10, 10, 65, 15));
-        pnlLoggedIn.add(lblUsername, new XYConstraints(65, 10, 80, 15));
-        pnlLoggedIn.add(btnLogout, new XYConstraints(695, 5, 90, 20));
         
+        this.getContentPane().add(userP);
         this.getContentPane().add(ntp);
-        this.getContentPane().add(pnlLoggedIn, null);
+        
         pnlSecondary.add(btnSave, new XYConstraints(655, 0, 130, 70));
         pnlSecondary.add(btnAnnuleren, new XYConstraints(10, 0, 130, 70));
         this.getContentPane().add(pnlSecondary, null);
