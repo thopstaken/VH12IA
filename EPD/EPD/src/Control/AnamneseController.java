77ef832c73c9dae9717b8187ad2a4dcb8205355c
Maurits Buijs <maurits.buijs@gmail.com>
2013-05-28 12:14:46 +0200

    Nieuwe klassen toegevoegd volgens klassendiagram

---
+++ EPD/EPD/src/Control/AnamneseController.java
@@ -0,0 +1,7 @@
+package Control;
+
+public class AnamneseController {
+    public AnamneseController() {
+        super();
+    }
+}
---
+++ EPD/EPD/src/Control/PatientController.java
@@ -0,0 +1,7 @@
+package Control;
+
+public class PatientController {
+    public PatientController() {
+        super();
+    }
+}
