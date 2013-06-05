70e187e2318dcb66266ada45da2bfb5f7d3ec979
Armando Leyba <armandoregilio@hotmail.com>
2013-06-05 16:07:58 +0200

    TimeLine GUI almost done

--- EPD/EPD/src/Boundary/Timeline.java
+++ EPD/EPD/src/Boundary/Timeline.java
@@ -1,18 +1,30 @@
 package Boundary;
 
+import Boundary.Common.ModifiedFlowLayout;
 import Boundary.Common.Userpanel;
 
 import Control.GUIController;
 
 import Entity.Patient;
 
+import com.sun.org.apache.bcel.internal.generic.NEW;
+
 import java.awt.BorderLayout;
 
+import java.awt.Color;
 import java.awt.Container;
 
+import java.awt.Dimension;
+import java.awt.ScrollPane;
+
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.border.LineBorder;
 
 public class Timeline extends JFrame {
     
@@ -38,21 +50,46 @@ public class Timeline extends JFrame {
     
     private void init(){
         setSize(800, 600);
+        setResizable(false);
         
         //infopanel
         infoPanel = new JPanel();
+        infoPanel.setBorder(BorderFactory.createLineBorder(Color.black));
+        //pPanel
         JPanel pPanel = new JPanel();
-        JLabel lblPatientNaam = new JLabel(mPatient.getFirstName());
+        pPanel.setLayout(new BoxLayout(pPanel, BoxLayout.Y_AXIS));
+        JLabel lblPatientNaam = new JLabel("Patiëntnaam: " + mPatient.getFirstName());
         pPanel.add(lblPatientNaam);
-        JLabel lblPatientGbDatum = new JLabel(mPatient.getDateOfBirth());
+        JLabel lblPatientGbDatum = new JLabel("Geboortedatum: " + mPatient.getDateOfBirth());
         pPanel.add(lblPatientGbDatum);
-        JLabel lblPatientNr = new JLabel(mPatient.getPatientNumber());
+        JLabel lblPatientNr = new JLabel("Patiëntnummer: " + mPatient.getPatientNumber());
         pPanel.add(lblPatientNr);
+        //btnPanel
+        JPanel btnPanel = new JPanel();
+        JButton btnAnamnese = new JButton("Anamnese Indienen");
+        btnPanel.add(btnAnamnese);
+        JButton btnVitaleFnc = new JButton("Vitale Functies Overzicht");
+        btnPanel.add(btnVitaleFnc);
+        JButton btnRapport = new JButton("Rapport ophalen");
+        btnPanel.add(btnRapport);
+        JButton btnOpdracht = new JButton("Opdracht indienen");
+        btnPanel.add(btnOpdracht);
         infoPanel.add(pPanel, BorderLayout.WEST);
+        infoPanel.add(btnPanel, BorderLayout.CENTER);
+        
+        //contentPanel
+        contentPanel = new JPanel();
+        contentPanel.setLayout(new ModifiedFlowLayout());
+        contentPanel.setBorder(BorderFactory.createLineBorder(Color.black));
+        contentPanel.setPreferredSize(new Dimension(750, 465));
+        JScrollPane sPane = new JScrollPane(contentPanel, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+                            JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        sPane.getVerticalScrollBar().setUnitIncrement(16);
         
         cp = getContentPane();        
         cp.add(mUserPanel, BorderLayout.NORTH);
         cp.add(infoPanel, BorderLayout.CENTER);
+        cp.add(sPane, BorderLayout.SOUTH);
         
     }
     
