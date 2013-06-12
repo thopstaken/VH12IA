11442e0a034213b938ba3539d929541504359473
Sietse Trommelen <strommelen@hotmail.com>
2013-06-12 11:04:53 +0200

    Rapportage
    Rapportage verder ingevuld, zover het kan. Veel data niet beschikbaar.

--- EPD/EPD/src/Entity/BloedDruk.java
+++ EPD/EPD/src/Entity/BloedDruk.java
@@ -5,6 +5,8 @@
 package Entity;
 
 
+import Control.PatientController;
+
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -13,67 +15,66 @@ import java.util.Date;
  *
  * @author Jan
  */
-public class BloedDruk{
-    
+public class BloedDruk {
+
     ArrayList onderdruk = new ArrayList<BloodPressureMeasurement>();
     ArrayList bovendruk = new ArrayList<BloodPressureMeasurement>();
-    ArrayList Saturatie = new ArrayList<SaturationMeasurement>();    
+    ArrayList Saturatie = new ArrayList<SaturationMeasurement>();
     int PatientID;
     Date date;
     String behandelaar;
-    
-    
-    public BloedDruk(){       
-        
-    }
-    
-    public void addOnderDrukValue(int value,Calendar valueb){
-        System.out.println("Onderdruk" + value);
-        onderdruk.add(new BloodPressureMeasurement(value,valueb) );       
-    }
-    
-    public void addBovenDrukValue(int value,Calendar valueb){
-        System.out.println("Bovendruk"+ value);
-        bovendruk.add(new BloodPressureMeasurement(value,valueb) );       
-    }
-    
-    public void addSaturationValue(int value,Calendar valueb){
-        System.out.println("Saturation"+ value);
-        Saturatie.add(new SaturationMeasurement(value,valueb) );       
+
+
+    public BloedDruk() {
+
     }
 
-    
+    public void addOnderDrukValue(int value, Calendar valueb) {
+        System.out.println("Onderdruk" + value);
+        onderdruk.add(new BloodPressureMeasurement(value, valueb));
+    }
+
+    public void addBovenDrukValue(int value, Calendar valueb) {
+        System.out.println("Bovendruk" + value);
+        bovendruk.add(new BloodPressureMeasurement(value, valueb));
+    }
+
+    public void addSaturationValue(int value, Calendar valueb) {
+        System.out.println("Saturation" + value);
+        Saturatie.add(new SaturationMeasurement(value, valueb));
+    }
+
+
     public void addValue(actionOnRapport type, Object valueA, Object valueB) {
-       
-       if(type == actionOnRapport.AddOnderDruk_BloedDruk ||type == actionOnRapport.AddBovenDruk_BloedDruk  ){
+
+        if (type == actionOnRapport.AddOnderDruk_BloedDruk ||
+            type == actionOnRapport.AddBovenDruk_BloedDruk) {
             int value = (Integer)valueA;
             Calendar valueb = (Calendar)valueB;
-            if(type == actionOnRapport.AddOnderDruk_BloedDruk ){
-                   addOnderDrukValue(value,valueb);
-            }           
-            else if(type == actionOnRapport.AddBovenDruk_BloedDruk ){           
-                    addBovenDrukValue(value,valueb);
+            if (type == actionOnRapport.AddOnderDruk_BloedDruk) {
+                addOnderDrukValue(value, valueb);
+            } else if (type == actionOnRapport.AddBovenDruk_BloedDruk) {
+                addBovenDrukValue(value, valueb);
+            } else if (type == actionOnRapport.AddSaturation) {
+                addBovenDrukValue(value, valueb);
             }
-            else if(type == actionOnRapport.AddSaturation ){           
-                    addBovenDrukValue(value,valueb);
-            }
-       }       
+        }
     }
 
-   
+
     public void open() {
-         BloodpressureChart chart =  new BloodpressureChart();
-         chart.setDiastolicList(onderdruk);
-         chart.setSystolicList(bovendruk);         
-         chart.saveToPNG("C:\\Temp");
+        BloodpressureChart chart = new BloodpressureChart(new Patient());
+        chart.setDiastolicList(onderdruk);
+        chart.setSystolicList(bovendruk);
+        chart.saveAndOpenPdf("C:\\Temp");
     }
-    
-  
+
+
     public void openExcel() {
-         BloodpressureChart chart =  new BloodpressureChart();
-         chart.setDiastolicList(onderdruk);
-         chart.setSystolicList(bovendruk);         
-         chart.saveToXsls("C:\\Temp");
+        BloodpressureChart chart = new BloodpressureChart(new Patient());
+        chart.setDiastolicList(onderdruk);
+        chart.setSystolicList(bovendruk);
+        chart.saveAndOpenXlsx("C:\\Temp");
     }
 
     public void setDate(Date date) {
@@ -95,7 +96,7 @@ public class BloedDruk{
     public enum actionOnRapport{
         AddOnderDruk_BloedDruk,
         AddBovenDruk_BloedDruk,
-        AddSaturation
+        AddSaturation;
     }
-    
+
 }
