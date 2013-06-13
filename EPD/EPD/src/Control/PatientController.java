d4ccc19acd6ecf9b2c069b1345c739a654b9e06e
Sietse Trommelen <strommelen@hotmail.com>
2013-06-13 12:20:19 +0200

    Delete patient
    Delete patient via webservice

--- EPD/EPD/src/Control/PatientController.java
+++ EPD/EPD/src/Control/PatientController.java
@@ -1,6 +1,8 @@
 package Control;
 
-import Entity.InsertPatient.Execute_ptt;
+import Entity.DeletePatient.DAPatient;
+import Entity.DeletePatient.DeletePatientService;
+
 import Entity.InsertPatient.InsertPatient;
 
 import Entity.Patient;
@@ -38,7 +40,7 @@ public class PatientController {
     public Object[][] getPatientList() {
 
         Object[][] data = new Object[patientList.size()][8];
-        
+
         SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
 
         for (int i = 0; i < patientList.size(); i++) {
@@ -46,7 +48,8 @@ public class PatientController {
             data[i][1] = patientList.get(i).getSurName();
             data[i][2] = patientList.get(i).getFirstName();
             data[i][3] = patientList.get(i).getDepartmentId();
-            data[i][4] = sdf.format(patientList.get(i).getDateOfBirth().getTime());
+            data[i][4] =
+                    sdf.format(patientList.get(i).getDateOfBirth().getTime());
             data[i][5] = patientList.get(i).getGender();
             data[i][6] = "Haal op uit anamnese.";
             data[i][7] = "Haal op uit anamnese.";
@@ -54,7 +57,7 @@ public class PatientController {
 
         return data;
     }
-    
+
     /**
      * Transforms Pantient objects to table data.
      * @return table data for GUI.
@@ -62,42 +65,42 @@ public class PatientController {
     public Object[][] getPatientList(String search) {
 
         Object[][] data = new Object[patientList.size()][8];
-        
+
         int j = 0;
-        
+
         SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
-        
+
         for (int i = 0; i < patientList.size(); i++) {
             if (patientList.get(i).getPatientNumber().contains(search) ||
                 patientList.get(i).getSurName().contains(search) ||
                 patientList.get(i).getFirstName().contains(search) ||
-                sdf.format(patientList.get(i).getDateOfBirth().getTime()).contains(search)
-                ) {
-                
+                sdf.format(patientList.get(i).getDateOfBirth().getTime()).contains(search)) {
+
                 data[j][0] = patientList.get(i).getPatientNumber();
                 data[j][1] = patientList.get(i).getSurName();
                 data[j][2] = patientList.get(i).getFirstName();
                 data[j][3] = patientList.get(i).getDepartmentId();
-                data[j][4] = sdf.format(patientList.get(i).getDateOfBirth().getTime());
+                data[j][4] =
+                        sdf.format(patientList.get(i).getDateOfBirth().getTime());
                 data[j][5] = patientList.get(i).getGender();
                 data[j][6] = "Haal op uit anamnese.";
                 data[j][7] = "Haal op uit anamnese.";
-                
+
                 j++;
             }
         }
-        
-        //zorgen dat de nieuwe lijst wordt gevuld met de gefilterde data. 
+
+        //zorgen dat de nieuwe lijst wordt gevuld met de gefilterde data.
         //dit is omdat de data lijst nog de verkeerde lengte heeft
         Object[][] data2 = new Object[j][8];
-        
+
         for (int i = 0; i < j; i++) {
             data2[i] = data[i];
         }
 
         return data2;
     }
-    
+
     public Patient getPatientById(int patientId) {
         Patient patient = null;
         for (Patient p : patientList) {
@@ -107,7 +110,7 @@ public class PatientController {
         }
         return patient;
     }
-    
+
     public Patient getPatientByNumber(String patientNumber) {
         Patient patient = null;
         for (Patient p : patientList) {
@@ -119,9 +122,10 @@ public class PatientController {
     }
 
     public void createDummiePatienten() {
-        addPatient("1", "Maurits", "", "Buijs", Calendar.getInstance(), "man", 1, 1, 1, 1);
-        addPatient("2", "Dave", "Duckface", "Lambregts", Calendar.getInstance(), "man",
-                   0, 0, 0, 0);
+        addPatient("1", "Maurits", "", "Buijs", Calendar.getInstance(), "man",
+                   1, 1, 1, 1);
+        addPatient("2", "Dave", "Duckface", "Lambregts",
+                   Calendar.getInstance(), "man", 0, 0, 0, 0);
     }
 
 
@@ -131,7 +135,7 @@ public class PatientController {
                               int overleden, int accountId, int afdelingId,
                               int accountActief) {
         Patient p = new Patient();
-        
+
         SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
 
         //p.setPatientId(patientId);
@@ -147,10 +151,10 @@ public class PatientController {
         p.setActive(accountActief);
 
 
-            Calendar cal = Calendar.getInstance();
-            cal.setTime(geboortedatum.getTime());
-            p.setDateOfBirth(cal);
-        
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(geboortedatum.getTime());
+        p.setDateOfBirth(cal);
+
         patientList.add(p);
 
         this.dbAction("insert", p);
@@ -184,8 +188,8 @@ public class PatientController {
     public Patient checkPatient(String patientNr, String voorNaam,
                                 String tussenVoegsel, String achterNaam,
                                 Calendar geboortedatum, String geslacht,
-                                int overleden, int accountId,
-                                int afdelingId, int accountActief) {
+                                int overleden, int accountId, int afdelingId,
+                                int accountActief) {
         for (Patient p : patientList) {
             if (p.getPatientNumber().equals(patientNr)) {
                 //Patient bestaat al
@@ -215,12 +219,14 @@ public class PatientController {
                 patient.getGender() + "','" + patient.getDeceased() + "','" +
                 patient.getUserId() + "','" + patient.getDepartmentId() +
                 "','" + patient.getActive() + "');";*/
-            
-           // pc.insertAction(query);
+
+            // pc.insertAction(query);
             InsertPatient insertPatient = new InsertPatient();
-            Execute_ptt ptt = insertPatient.getExecute_pt();
-           
-            Entity.InsertPatient.Patient patientWs = new  Entity.InsertPatient.Patient();
+            Entity.InsertPatient.Execute_ptt ptt =
+                insertPatient.getExecute_pt();
+
+            Entity.InsertPatient.Patient patientWs =
+                new Entity.InsertPatient.Patient();
             patientWs.setPatientnummer(new BigDecimal(patient.getPatientNumber()));
             patientWs.setVoornaam(patient.getFirstName());
             patientWs.setTussenvoegsel(patient.getPrefix());
@@ -231,31 +237,44 @@ public class PatientController {
             patientWs.setUserId(new BigDecimal(patient.getUserId()));
             patientWs.setAfdelingId(new BigDecimal(patient.getDepartmentId()));
             patientWs.setActiveInd(new BigDecimal(1));
-            
+
             try {
-            ptt.execute(patientWs);
-            }
-            catch(Exception e) {
+                ptt.execute(patientWs);
+            } catch (Exception e) {
                 e.printStackTrace();
             }
-            
+
         } else if (dbAction.equals("update")) {
             //TODO db update action
         } else if (dbAction.equals("delete")) {
-            //TODO db delete action
+            DeletePatientService dps = new DeletePatientService();
+            Entity.DeletePatient.Execute_ptt ptt = dps.getExecute_pt();
+            DAPatient dPatient = new DAPatient();
+
+            dPatient.setPatientid(new BigDecimal(patient.getPatientId()));
+            dPatient.setActiveInd(new BigDecimal(0));
+
+            try {
+                ptt.execute(dPatient);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
         }
     }
-    
-    public static XMLGregorianCalendar toXMLGregorianCalendar(Date date){
-            GregorianCalendar gCalendar = new GregorianCalendar();
-            gCalendar.setTime(date);
-            XMLGregorianCalendar xmlCalendar = null;
-            try {
-                xmlCalendar = DatatypeFactory.newInstance().newXMLGregorianCalendar(gCalendar);
-            } catch (DatatypeConfigurationException ex) {
-                ex.printStackTrace();
-            }
-            return xmlCalendar;
+
+    public static XMLGregorianCalendar toXMLGregorianCalendar(Date date) {
+        GregorianCalendar gCalendar = new GregorianCalendar();
+        gCalendar.setTime(date);
+        XMLGregorianCalendar xmlCalendar = null;
+        try {
+            xmlCalendar =
+                    DatatypeFactory.newInstance().newXMLGregorianCalendar(gCalendar);
+        } catch (DatatypeConfigurationException ex) {
+            ex.printStackTrace();
         }
+        return xmlCalendar;
+    }
 
 }
